f59d3742126f65a2ab043f0089796f9e60d88f25
Marko Velkavrh <care000@gmail.com>
2015-04-05 14:43:19 +0200

    popravk

--- dna.py
+++ dna.py
@@ -2,6 +2,7 @@ infile = open('C:/Users/metka/PycharmProjects/Phyton_Vaje/vaja6/dna.txt')
 outfile = open('C:/Users/metka/PycharmProjects/Phyton_Vaje/vaja6/krivec.txt', 'w')
 outfile.write('Krivec je Miha!\n')
 
+
 replacements = {'CCAGCAATCGC':'Crna', 'GCCAGTGCCG':'Rjava', 'TTAGCTATCGC':'Korencek', "GCCACGG" : "Kvadraten", "ACCACAA" : "Okrogel", "AGGCCTCA" : "Ovalen", "TTGTGGTGGC" : "Modra", "GGGAGGTGGC" : "Zelena", "AAGTAGTGAC" : "Rjava", "TGCAGGAACTTC" : "Moski", "TGAAGGACCTTC" : "Zenska", "AAAACCTCA" : "Belec", "CGACTACAG" : "Crnec", "CGCGGGCCG" : "Azijec"}
 
 for line in infile:
